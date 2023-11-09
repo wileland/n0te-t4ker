8716a92dfdb6427bb1741a9954002570cd56bcd2
wileland <wileland7@gmail.com>
2023-11-08 18:11:12 -0600

    merged index and server js- deleted index

--- README.md
+++ README.md
@@ -21,3 +21,22 @@ WHEN I click on an existing note in the list in the left-hand column
 THEN that note appears in the right-hand column and a "New Note" button appears in the navigation
 WHEN I click on the "New Note" button in the navigation at the top of the page
 THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column and the button disappears
+
+NOTE T4...
+├── db
+│   └── db.json
+├── node_modules
+├── public
+│   ├── client.js
+│   ├── index.html
+│   ├── index.js
+│   ├── notes.html
+│   └── styles.css
+├── Routes
+│   ├── API routes
+│   └── HTML routes
+├── .gitignore
+├── package-lock.json
+├── package.json
+├── README.md
+└── server.js
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { v4: uuidv4 } = require('uuid');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use(express.json()); // Use built-in middleware for json
 app.use(express.static('public')); // Serve static files from the public directory
 
 // GET /notes: Return the notes.html file
@@ -26,53 +26,44 @@ app.get('/api/notes', (req, res) => {
   }
 });
 
-
 // POST /api/notes: Receive a new note to add to the db.json file, and return the new note
 app.post('/api/notes', (req, res) => {
   const newNote = {
-    id: uuidv4(),
+    id: uuidv4(), // Use UUID for unique identifier
     title: req.body.title,
     text: req.body.text,
   };
 
-  fs.readFile(path.join(__dirname, 'db.json'), 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({ error: 'An error occurred while reading notes.' });
-    }
-
+  try {
+    const data = fs.readFileSync(path.join(__dirname, 'db.json'), 'utf8');
     const notes = JSON.parse(data);
     notes.push(newNote);
-
-    fs.writeFile(path.join(__dirname, 'db.json'), JSON.stringify(notes, null, 2), (err) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).json({ error: 'An error occurred while saving the new note.' });
-      }
-      res.json(newNote);
-    });
-  });
+    fs.writeFileSync(path.join(__dirname, 'db.json'), JSON.stringify(notes, null, 2));
+    res.json(newNote);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while saving the new note.' });
+  }
 });
 
 // DELETE /api/notes/:id: Remove a note by ID from the db.json file
 app.delete('/api/notes/:id', (req, res) => {
-  fs.readFile(path.join(__dirname, 'db.json'), 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({ error: 'An error occurred while reading notes.' });
-    }
-
+  try {
+    const data = fs.readFileSync(path.join(__dirname, 'db.json'), 'utf8');
     let notes = JSON.parse(data);
+    const notesBeforeDeletion = notes.length;
     notes = notes.filter((note) => note.id !== req.params.id);
 
-    fs.writeFile(path.join(__dirname, 'db.json'), JSON.stringify(notes, null, 2), (err) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).json({ error: 'An error occurred while deleting the note.' });
-      }
-      res.json({ message: 'Note deleted successfully.' });
-    });
-  });
+    if (notes.length === notesBeforeDeletion) {
+      return res.status(404).json({ error: 'Note not found.' });
+    }
+
+    fs.writeFileSync(path.join(__dirname, 'db.json'), JSON.stringify(notes, null, 2));
+    res.json({ message: 'Note deleted successfully.' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while deleting the note.' });
+  }
 });
 
 // Listen on the specified port
